9d7b902bcf37b1fbbb191aa08debfb0537974482
Guy Harrison <guy@southbanksoftware.com>
2020-05-13 18:10:43 +1000

    profiler query

--- scripts/mongoTuning.js
+++ scripts/mongoTuning.js
@@ -1,65 +1,54 @@
 // Master utility script for the Apress book "MongoDB Performance Tuning"
 
-/* eslint no-var: 0 */
+/* eslint no-var: 1 */
 /* eslint no-prototype-builtins: 0 */
 /* eslint camelcase: 0 */
 /* eslint prefer-arrow-callback: 0 */
 /* eslint object-shorthand: 0 */
 /* eslint vars-on-top: 0 */
-/* eslint no-unused-vars: 0 */
-/* eslint no-plusplus: 0 */
-
-// Master utility script for the Apress book "MongoDB Performance Tuning"
-
-/* eslint no-var: 0 */
-/* eslint no-prototype-builtins: 0 */
-/* eslint camelcase: 0 */
-/* eslint prefer-arrow-callback: 0 */
-/* eslint object-shorthand: 0 */
-/* eslint vars-on-top: 0 */
-/* eslint no-unused-vars: 0 */
+/* eslint no-unused-vars: 1 */
 /* eslint no-plusplus: 0 */
 
 var mongoTuning = {};
 
- mongoTuning.prepExplain = (explainInput) => {
+mongoTuning.prepExplain = (explainInput) => {
   // Takes as input explain output in one of the follow formats:
   // A fully explain JSON document, in which case emits winningPlan
   // An explain() cursor in which case, extracts the winningPlan from the cursor
   // A specific plan step in which case just returns that
-  let type;
+
   const keys = Object.keys(explainInput);
- // printjson(keys);
+  // printjson(keys);
   if (keys.includes('queryPlanner')) {
     // This looks like a top level Explain
     return explainInput.queryPlanner.winningPlan;
   } else if (keys.includes('hasNext')) {
     // This looks like a cursor
     if (explainInput.hasNext()) {
-      return (mongoTuning.prepExplain(explainInput.next()));
+      return mongoTuning.prepExplain(explainInput.next());
     }
-      return ({ok:0, error:'No plan found'});
+    return { ok: 0, error: 'No plan found' };
   } else if (keys.includes('stage')) {
     // This looks like an actual plan
-    return (explainInput);
+    return explainInput;
   }
-    return ({ok:0, error:'No plan found'});
+  return { ok: 0, error: 'No plan found' };
 };
 
 mongoTuning.quickExplain = (inputPlan) => {
   // Takes as input an explain Plan.  Emits a simplified
   // version of that plan
   const explainPlan = mongoTuning.prepExplain(inputPlan);
-  var stepNo = 1;
+  let stepNo = 1;
 
-  var printSpaces = function(n) {
-    var s = '';
-    for (var i = 1; i < n; i++) {
+  const printSpaces = function(n) {
+    let s = '';
+    for (let i = 1; i < n; i++) {
       s += ' ';
     }
     return s;
   };
-  var printInputStage = function(step, depth) {
+  const printInputStage = function(step, depth) {
     if ('inputStage' in step) {
       printInputStage(step.inputStage, depth + 1);
     }
@@ -77,13 +66,34 @@ mongoTuning.quickExplain = (inputPlan) => {
   printInputStage(explainPlan, 1);
 };
 
+mongoTuning.prepExecutionStats = (explainInput) => {
+  // Takes as input explain output in one of the follow formats:
+  // A fully explain JSON document, in which case emits executionStats
+  // An explain() cursor in which case, extracts the exectionStats from the cursor
 
-mongoTuning.executionStats = (execStats) => {
-  var stepNo = 1;
+
+  const keys = Object.keys(explainInput);
+
+
+  if (keys.includes('executionStats')) {
+    // This looks like a top level Explain
+    return explainInput.executionStats;
+  } else if (keys.includes('hasNext')) {
+    // This looks like a cursor
+
+    if (explainInput.hasNext()) {
+      return mongoTuning.prepExecutionStats(explainInput.next());
+    }
+  } else return { ok: 0, error: 'No executionStats found' };
+};
+
+mongoTuning.executionStats = (execStatsIn) => {
+  const execStats = mongoTuning.prepExecutionStats(execStatsIn);
+  let stepNo = 1;
   print('\n');
-  var printSpaces = function(n) {
-    var s = '';
-    for (var i = 1; i < n; i++) {
+  const printSpaces = function(n) {
+    let s = '';
+    for (let i = 1; i < n; i++) {
       s += ' ';
     }
     return s;
@@ -97,7 +107,7 @@ mongoTuning.executionStats = (execStats) => {
         printInputStage(inputStage, depth + 1);
       });
     }
-    var extraData = '(';
+    let extraData = '(';
     if ('indexName' in step) extraData += ' ' + step.indexName;
     if ('executionTimeMillisEstimate' in step) {
       extraData += ' ms:' + step.executionTimeMillisEstimate;
@@ -117,3 +127,32 @@ mongoTuning.executionStats = (execStats) => {
     execStats.totalDocsExamined
   );
 };
+
+mongoTuning.profileQuery = () => {
+  let profileQuery = db.system.profile.aggregate([
+    {
+      $group: {
+        _id: { cursorid: '$cursorid' },
+        count: { $sum: 1 },
+        'queryHash-max': { $max: '$queryHash' },
+        'millis-sum': { $sum: '$millis' },
+        'ns-max': { $max: '$ns' }
+      }
+    },
+    {
+      $group: {
+        _id: {
+          queryHash: '$queryHash-max',
+          collection: '$ns-max'
+        },
+        count: { $sum: 1 },
+        millis: { $sum: '$millis-sum' }
+      }
+    },
+    { $sort: { millis: -1 } },
+    { $limit: 10 }
+  ]);
+  return profileQuery;
+};
+
+ 
